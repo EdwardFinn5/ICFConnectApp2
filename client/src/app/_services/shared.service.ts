613bf2252afc075912ee1115e3283d30830c1fea
Ed Finn <ed@iowacollegefoundation.org>
2021-09-09 07:38:14 -0500

    added shared services

--- client/src/app/_services/shared.service.ts
+++ client/src/app/_services/shared.service.ts
@@ -9,5 +9,45 @@ export class SharedService {
   readonly APIUrl = 'https://localhost:5001/api';
   readonly PhotoUrl = 'https://localhost:5001/Photos';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getMajorList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + 'majors');
+  }
+
+  addMajor(val: any) {
+    return this.http.post(this.APIUrl + '/majors', val);
+  }
+
+  updateMajor(val: any) {
+    return this.http.put(this.APIUrl + '/majors', val);
+  }
+
+  deleteMajor(val: any) {
+    return this.http.delete(this.APIUrl + '/majors/', val);
+  }
+
+  getUserList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + 'users');
+  }
+
+  addUser(val: any) {
+    return this.http.post(this.APIUrl + '/users', val);
+  }
+
+  updateUser(val: any) {
+    return this.http.put(this.APIUrl + '/users', val);
+  }
+
+  deleteUser(val: any) {
+    return this.http.delete(this.APIUrl + '/users/' + val);
+  }
+
+  uploadPhoto(val: any) {
+    return this.http.post(this.APIUrl + '/users/SaveFile', val);
+  }
+
+  getAllMajorNames(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/majors');
+  }
 }
